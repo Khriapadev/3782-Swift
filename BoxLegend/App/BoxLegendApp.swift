b18d0ae2c3dd46bb7c65dadb4a43a729d6a2b386
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-02 12:14:13 +0200

    init

---
+++ BoxLegend/App/BoxLegendApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxLegendApp.swift
+//  BoxLegend
+//
+//  Created by Serhii Babchuk on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BoxLegendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BLLaunchView()
+        }
+    }
+}
